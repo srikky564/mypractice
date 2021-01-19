d13c555e7f6b618791eed30a10495cddcd328f78
bsriaknth-pk <bsrikanth@prokarma.com>
2021-01-19 20:56:22 +0530

    Updates

---
+++ precache-manifest.b05d5f99d3867f47cdf90480cd3270ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b626ed191d88a9f34a6409974c99c25b",
+    "url": "/mypractice/index.html"
+  },
+  {
+    "revision": "15ba7ab7a25a813c10b9",
+    "url": "/mypractice/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "723594ad26c38ad940a6",
+    "url": "/mypractice/static/css/main.208d5075.chunk.css"
+  },
+  {
+    "revision": "15ba7ab7a25a813c10b9",
+    "url": "/mypractice/static/js/2.b25251a9.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/mypractice/static/js/2.b25251a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723594ad26c38ad940a6",
+    "url": "/mypractice/static/js/main.2f3eede6.chunk.js"
+  },
+  {
+    "revision": "02588ec968f21056126d",
+    "url": "/mypractice/static/js/runtime-main.cc9ecebd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/mypractice/static/media/SEGA.12a3db8b.woff"
+  }
+]);
